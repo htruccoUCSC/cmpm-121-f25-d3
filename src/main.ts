f8763bd29a729eb2717c8a3c99af83e63e7434f6
Sonny Trucco <htrucco@ucsc.edu>
2025-11-10 02:16:39 +0000

    Step 3 complete, displaying a basic leaflet map

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+// @deno-types="npm:@types/leaflet"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css"; // supporting style for Leaflet
+import "./style.css"; // student-controlled page style
+
+// Fix missing marker images
+import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
+
+const controlPanelDiv = document.createElement("div");
+controlPanelDiv.id = "controlPanel";
+document.body.append(controlPanelDiv);
+
+const mapDiv = document.createElement("div");
+mapDiv.id = "map";
+document.body.append(mapDiv);
+
+const statusPanelDiv = document.createElement("div");
+statusPanelDiv.id = "statusPanel";
+document.body.append(statusPanelDiv);
+
+// Our classroom location
+const CLASSROOM_LATLNG = leaflet.latLng(
+  36.997936938057016,
+  -122.05703507501151,
+);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(mapDiv, {
+  center: CLASSROOM_LATLNG,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
